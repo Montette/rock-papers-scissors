19cebe7793135a1734d2642a956f9199f98a65bd
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2018-03-18 19:46:58 +0100

    add img with hands showing players choice

--- js/script.js
+++ js/script.js
@@ -29,12 +29,33 @@ var playerPickElem = document.getElementById('js-playerPick'),
     computerResultElem = document.getElementById('js-computerResult');
 
 pickRock.addEventListener('click', function () {
+    var imgHand = document.createElement("img");
+    imgHand.setAttribute("src", "img/hands/rockLeft.png");
+    while (playerPickElem.firstChild) {
+    playerPickElem.removeChild(playerPickElem.firstChild);
+}
+//    playerPickElem.removeChild(imgHand);
+    playerPickElem.appendChild(imgHand);
     playerPick('rock')
 });
 pickPaper.addEventListener('click', function () {
+    var imgHand = document.createElement("img");
+    imgHand.setAttribute("src", "img/hands/paperLeft.png");
+     while (playerPickElem.firstChild) {
+    playerPickElem.removeChild(playerPickElem.firstChild);
+     }
+//     playerPickElem.removeChild(imgHand);
+    playerPickElem.appendChild(imgHand);
     playerPick('paper')
 });
 pickScissors.addEventListener('click', function () {
+    var imgHand = document.createElement("img");
+    imgHand.setAttribute("src", "img/hands/scissorsLeft.png");
+     while (playerPickElem.firstChild) {
+    playerPickElem.removeChild(playerPickElem.firstChild);
+     }
+//    playerPickElem.removeChild(imgHand);
+    playerPickElem.appendChild(imgHand);
     playerPick('scissors')
 });
 
@@ -75,9 +96,14 @@ function newGame() {
 
 function playerPick(playerPick) {
     var computerPick = getComputerPick();
+    
+//    var img = document.createElement("img");
+//    img.setAttribute("src", "rockLeft.png");
+    
+//    playerPickElem.appendChild(leftRock);
 
-    playerPickElem.innerHTML = playerPick;
-    computerPickElem.innerHTML = computerPick;
+//    playerPickElem.innerHTML = playerPick;
+//    computerPickElem.innerHTML = computerPick;
 
     checkRoundWinner(playerPick, computerPick);
     setGamePoints();
@@ -85,7 +111,31 @@ function playerPick(playerPick) {
 
 function getComputerPick() {
     var possiblePicks = ['rock', 'paper', 'scissors'];
-    return possiblePicks[Math.floor(Math.random() * 3)];
+    var computerPick =  possiblePicks[Math.floor(Math.random() * 3)];
+    
+    var imgHand = document.createElement("img");
+    
+    switch (computerPick){
+        case 'paper': 
+    imgHand.setAttribute("src", "img/hands/paperRight.png");
+    break;
+        case 'rock':
+    imgHand.setAttribute("src", "img/hands/rockRight.png");
+    break;
+        case 'scissors':
+    imgHand.setAttribute("src", "img/hands/scissorsRight.png");
+    break;
+    }
+        
+     while (computerPickElem.firstChild) {
+    computerPickElem.removeChild(computerPickElem.firstChild);
+     }
+//     playerPickElem.removeChild(imgHand);
+    computerPickElem.appendChild(imgHand);
+    
+    return computerPick;
+    
+    
 }
 
 function checkRoundWinner(playerPick, computerPick) {
@@ -131,6 +181,6 @@ function setGamePoints() {
 function resetGame() {
     playerPickElem.innerHTML = "Player Selection";
 	computerPickElem.innerHTML = "Computer Selection";
-	playerResultElem.innerHTML = "Player Score";
-    computerResultElem.innerHTML = "Computer Score";   
+	playerResultElem.innerHTML = "";
+    computerResultElem.innerHTML = "";   
 }
